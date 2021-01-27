0e9330f2de5ec822f0f2504da1366d1c4744069c
Austin Stiffler <austin.stiffler@outlook.com>
2021-01-27 11:42:30 -0500

    First homework commit

---
+++ Homework/hw03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace hw03
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+        long factorial (int dataValue)
+            {
+                if (dataValue == 1)
+                    return 1;
+                else
+                    return dataValue * factorial(dataValue - 1);
+            }
+        }
+    }
+}
